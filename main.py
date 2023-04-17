ddc5a2bea00e6e93b3be134927ac966e50aa8a5e
NQuiros12 <nicolasquiros12@gmail.com>
2023-04-17 11:36:06 -0300

    Add a function to alter tables and add the PK and FK

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import services as _services
-
-import os
-
-uri = "data/hired_employees_t.csv"
-table_name = os.path.basename(uri).replace(".csv","")
-_services.upload_csv(_services.read_csv(uri), table_name)
+file_paths = [f"data/{filename}.csv" for filename in _services.get_all_files("data")]
+#Re arrange the file paths in order to execute first with 'jobs' and 'departments' and after the
+# 'hired_employees' since the last one depends on the others.
+file_paths[1], file_paths[-1] = file_paths[-1], file_paths[1]
+from pathlib import Path
+# Bring all the files and upload to the sql database
+[_services.upload_csv(_services.read_csv(file_path),Path(file_path).stem ) for file_path in file_paths]
+[_services.add_constraints(Path(file_path).stem)for file_path in file_paths]
--- services.py
+++ services.py
@@ -1,19 +1,30 @@
 from db import Connector
 from fastapi import File, UploadFile
 import pandas as pd
-from sqlalchemy import create_engine
+import sqlalchemy as sa
+from sqlalchemy import create_engine, ForeignKey
+import os
 
-DB_USER ="root"
+DB_USER = "root"
 DB_PASSWORD = "micolash12"
 DB_HOST = "localhost"
-DATABASE ="globant_db"
+DATABASE = "globant_db"
 DB_PORT = 3306
-	
+connect_string = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DATABASE}?charset=utf8"
+engine = create_engine(connect_string)
 
-#connector = Connector(DATABASE, DB_HOST, DB_USER, DB_USER,DB_PASSWORD, DB_PORT)
-#Create the connection up and running
-#
-async def get_all(table_name:str)->list:
+# connector = Connector(DATABASE, DB_HOST, DB_USER, DB_USER,DB_PASSWORD, DB_PORT)
+# Create the connection up and running
+def get_all_files(dir: str) -> list:
+    # Return all files names from the data dir without extension
+    files = list(
+        map(lambda x: os.path.basename(x).replace(
+            ".csv", ""), os.listdir("data"))
+    )
+    return files
+
+
+async def get_all(table_name: str) -> list:
     try:
         connector.get_connection()
         cursor = connector.cursor(dictionary=True)
@@ -21,21 +32,36 @@ async def get_all(table_name:str)->list:
         rows = cursor.fetchall()
         return rows
     except mysql.connector.Error as e:
-        return JSONResponse(status_code = 500, content = { "message":"Error connecting to database"})
+        return JSONResponse(
+            status_code=500, content={"message": "Error connecting to database"}
+        )
 
-#Create the service of reading the csv
-def read_csv(file:UploadFile = File()) -> pd.DataFrame:
+
+# Create the service of reading the csv
+def read_csv(file: UploadFile = File()) -> pd.DataFrame:
     df = pd.read_csv(file)
     return df
-#Upload the file to the database
-def upload_csv(df:pd.DataFrame,table_name:str) ->None:
-	connect_string = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DATABASE}?charset=utf8'
-	engine = create_engine(connect_string)
-	# Use the built-in function 'to_sql' to write the dataframe to the database
-	df.to_sql(table_name, engine, if_exists='replace', index=False)
-	print("Csv upload to database")
-	return None
-def get_all_files(dir:str) -> list:
-    #Return all files names from the data dir without extension
-    files = list(map(lambda x: os.path.basename(x).replace(".csv",""),os.listdir("data")))
-    return files
+
+
+# Upload the file to the database (batch)
+def upload_csv(df: pd.DataFrame, table_name: str) -> None:
+    
+
+    # Use the built-in function 'to_sql' to write the dataframe to the database
+    df.to_sql(table_name, engine, if_exists="append",
+              index=False, chunksize=1000)
+    print(f"{table_name} csv upload to database")
+    return None
+def add_constraints(table_name:str)->None:
+    # Use the engine to write the primary key constraint to the database
+    with engine.connect() as con:
+        # and in the case of 'hired_employees' we add also a foreign key constraint
+        con.execute(sa.text(f"alter table {table_name} add primary key (id);"))
+        if ("hired_employees_t" in table_name):
+            con.execute(sa.text(
+                f"ALTER TABLE {table_name} ADD CONSTRAINT department_fk FOREIGN KEY(department_id) references departments_t(id)"
+            ))
+            con.execute(sa.text(
+                f"ALTER TABLE {table_name} ADD CONSTRAINT job_fk FOREIGN KEY(job_id) references jobs_t(id)"
+            ))
+        
